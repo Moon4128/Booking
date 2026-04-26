28a342ba1fb153f9bc4d0a85459e76ba3bb349ab
Moon4128 <167788546+Moon4128@users.noreply.github.com>
2026-04-26 19:34:03 +0300

    Add func

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
-    path('rooms/', views.room_list, name = 'room-list'),
+    path('', views.room_list, name = 'rooms'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,7 +3,24 @@ from .models import Room, Category, Booking
 
 def room_list(request):
     all_rooms = Room.objects.all()
+    categories = Category.objects.all()
+    
+    category_id = request.GET.get('category')
+    capacity = request.GET.get('capacity')
+    price_max = request.GET.get('price_max')
+    if category_id:
+        all_rooms = all_rooms.filter(category=category_id)
+    if capacity:
+        all_rooms = all_rooms.filter(capacity__gte=int(capacity))
+    if price_max:
+        all_rooms = all_rooms.filter(price__lte=float(price_max))
+
+
     context = {
-        'rooms': all_rooms
+        'rooms': all_rooms,
+        'categories': categories, 
+        'selected_capacity': capacity,
+        'selected_price_max': price_max,
+        'selected_category': category_id,
     }
     return render (request=request, template_name='core/room_list.html', context=context)
