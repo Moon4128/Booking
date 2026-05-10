758a280437d748893bd366f5c1fb972254b767ad
Ekaterina <ekaterinademi8@gmail.com>
2026-05-10 19:30:25 +0300

    Add news html parts and views

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,6 @@ from core import views
 
 urlpatterns = [
     path('', views.room_list, name = 'rooms'),
+    path('booking/<int:room_id>/', views.book_room, name = 'booking'),
+    path('my_bookings/', views.user_bookings, name = 'user_bookings'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Room, Category, Booking
+import datetime
 
 def room_list(request):
     all_rooms = Room.objects.all()
@@ -24,3 +25,44 @@ def room_list(request):
         'selected_category': category_id,
     }
     return render (request=request, template_name='core/room_list.html', context=context)
+
+def book_room(request, room_id):
+    room = Room.objects.get(pk=room_id)
+    context = {
+        'room': room,
+    }
+    if request.method == 'GET':
+        return render(request=request, template_name='core/booking_form.html', context=context)
+    elif request.method == 'POST':
+        start_date = datetime.strptime(request.POST.get('start_date'), '%Y-%m-%d').date()
+        end_date = datetime.strptime(request.POST.get('end_date'), '%Y-%m-%d').date()
+
+        user = request.user
+        room = Room.objects.get(pk=room_id)
+
+        if end_date <= start_date or start_date < str(datetime.now().date()):
+            return render(request=request, template_name='core/booking_form.html', context=context)
+        
+        if Booking.objects.filter(room=room, start_date__lte=end_date, end_date__gte=start_date).exists():
+            return render(request=request, template_name='core/booking_form.html', context=context)
+
+
+
+        Booking.objects.create(
+            user = user,
+            room = room,
+            start_date = start_date,
+            end_date = end_date
+        )
+
+        context = {
+            'bookings': Booking.objects.filter(user=user).order_by('-start_date')
+        }
+        return render(request=request, template_name='core/my_bookings.html', context=context)
+    
+def user_bookings(request):
+    user = request.user
+    context = {
+        'bookings': Booking.objects.filter(user=user).order_by('-start_date')
+    }
+    return render(request=request, template_name='core/my_bookings.html', context=context)
